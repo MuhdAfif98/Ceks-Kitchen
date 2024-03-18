2dc08abda9320f3db30f75cefa4f91f18ce92443
Muhammad Afif <44563274+MuhdAfif98@users.noreply.github.com>
2024-03-18 23:03:57 +0800

    set routing for navigation

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/my-recipe', function () {
+    return view('recipe.index.index');
+})->middleware(['auth', 'verified'])->name('my-recipe');;
+
+Route::get('/forum', function () {
+    return view('forum.index.index');
+})->middleware(['auth', 'verified'])->name('forum');
+
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.index.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
